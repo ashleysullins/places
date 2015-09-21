aace06d33e237700e029dd47a56762fae8c318f4
Ashley Sullins <ashley.sullins@gmail.com>
2015-09-21 11:16:40 -0700

    add ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-Example of Scripts
+$(document).ready(function() {  $("form#new-location").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedCountry = $("input#new-country").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedNotablePlace = $("input#new-notable-place").val();
+    var inputtedNotes = $("input#new-notes").val();
+    var newLocation = { country: inputtedCountry, state: inputtedState, city: inputtedCity, notablePlace: inputtedNotablePlace, notes: inputtedNotes };
 
-// var factorial = function(number) {
-//   if (number === 0) {
-//     return 1;
-//   } else {
-//
-//     return (number * factorial(number-1));
-//   }
-// }
-//
-//
-// $(document).ready(function() {
-//   $("form#factorial").submit(function(event) {
-//     var number= parseInt($("input#number").val());
-//     var result = factorial(number);
-//
-//       $(".number").text(number)
-//       $(".final").text(result)
-//        $("#result").show()
-//
-//
-//     event.preventDefault();
-//   });
-// });
+    $("ul#location").append("<li><span class='location'>" + newLocation.country + " " + newLocation.state + " " + newLocation.city + "</span></li>");
+    $("input#new-country").val("");
+    $("input#new-state").val("");
+    $("input#new-city").val("");
+    $("input#new-notable-place").val("");
+    $("input#new-notes").val("");
+  });
+});
+
+$(".location").last().click(function() {
+  $("#show-location").show();
+  $("#show-location h2").text(newLocation.country + " " + newLocation.state + " " + newLocation.city);
+  $(".country").text(newLocation.country);      $(".state").text(newLocation.state);
+  $(".city").text(newLocation.city);    $(".notable-place").text(newLocation.notablePlace);
+  $(".notes").text(newLocation.notes);
+});
