7950a5eec7ffede8a99d35545a4d12c11c38fe09
Ashley Sullins <ashley.sullins@gmail.com>
2015-09-21 09:25:53 -0700

    Add inital files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+Example of Scripts
+
+
+// var factorial = function(number) {
+//   if (number === 0) {
+//     return 1;
+//   } else {
+//
+//     return (number * factorial(number-1));
+//   }
+// }
+//
+//
+// $(document).ready(function() {
+//   $("form#factorial").submit(function(event) {
+//     var number= parseInt($("input#number").val());
+//     var result = factorial(number);
+//
+//       $(".number").text(number)
+//       $(".final").text(result)
+//        $("#result").show()
+//
+//
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+Example of spec
+
+// describe('factorial', function() {
+//   it("will take a number and multiply it by all other numbers less than it", function() {
+//     expect(factorial(5)).to.equal(120);
+//   });
+// });
